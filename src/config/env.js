60e969ca59b968af36622ec3c0961c81be87d2b2
Henrique Peluzio <hpeluzio@gmail.com>
2021-10-14 20:07:06 -0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,23 +1,26 @@
 // API_URL=http://10.0.2.2:3333
 // API_URL=https://api.bolaodasorte.online
 
-export const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://10.0.2.2:3333'
-    : 'https://api.bolaodasorte.online';
+export const API_URL = 'https://api.bolaodasorte.online';
+
+// export const API_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://10.0.2.2:3333'
+//     : 'https://api.bolaodasorte.online';
+
 export const PUBLIC_KEY = 'TEST-5cfa7891-ec91-488e-9f6a-a27cf73ddec6';
 
-// export const PAYMENT_METHODS_URL = 'https://paymentmethods.bolaodasorte.online';
-
-// export const CREATE_CARD_TOKEN_URL =
-//   'https://createcardtoken.bolaodasorte.online';
-
-export const PAYMENT_METHODS_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://10.0.2.2:4444'
-    : 'https://paymentmethods.bolaodasorte.online';
+export const PAYMENT_METHODS_URL = 'https://paymentmethods.bolaodasorte.online';
 
 export const CREATE_CARD_TOKEN_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://10.0.2.2:4445'
-    : 'https://createcardtoken.bolaodasorte.online';
+  'https://createcardtoken.bolaodasorte.online';
+
+// export const PAYMENT_METHODS_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://10.0.2.2:4444'
+//     : 'https://paymentmethods.bolaodasorte.online';
+
+// export const CREATE_CARD_TOKEN_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://10.0.2.2:4445'
+//     : 'https://createcardtoken.bolaodasorte.online';
